ce29a87b283c82ee3e1a759852b54a1556dbf298
Juan Soriano <juanky@novoda.com>
2022-09-21 21:33:01 -0500

    Add first iteration of Dreams API

---
+++ dreams.py
@@ -0,0 +1,46 @@
+import torch
+from huggingface_hub.commands.user import _login
+from diffusers import StableDiffusionPipeline
+from torch import autocast
+import io
+from fastapi import FastAPI, Response
+import asyncio
+from fastapi.middleware.cors import CORSMiddleware
+import nest_asyncio
+import uvicorn
+from huggingface_hub import HfFolder
+import os
+
+HfFolder.save_token(os.getenv('HUGGING_FACE'))
+
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16, use_auth_token=True)
+pipe = pipe.to("cuda")
+
+def dream(prompt: str):
+  with autocast("cuda"):
+    image = pipe(prompt, num_inference_steps=50)["sample"][0]  
+
+  byteIO = io.BytesIO()
+  image.save(byteIO, format='PNG')
+  return byteIO.getvalue()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get("/dreams")
+async def dreams(prompt: str):
+    return Response(content=dream(prompt), media_type="image/png")
+
+@app.get("/status")
+async def status():
+    return Response(status_code=200)
+
+nest_asyncio.apply()
+uvicorn.run(app, port=8000)
---
+++ run.sh
@@ -0,0 +1,8 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 12
+nvm use 12
+npm install -g localtunnel
+pip install -r requirements.txt
+python dreams.py & lt --port 8000 --subdomain novoda-dreams 
