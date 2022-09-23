6d47d1c7a2b5f1988d06062b7a5346fa7dfffde2
Juan Soriano <juanky@novoda.com>
2022-09-23 12:07:42 -0500

    Adds a pod-start-command

---
+++ pod-start-command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/novoda/dreams/main/dreams.py
+wget https://raw.githubusercontent.com/novoda/dreams/main/run.sh
+wget https://raw.githubusercontent.com/novoda/dreams/main/requirements.txt
+
+sh run.sh
