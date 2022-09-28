a559088a94c7ce5943cfb495e1c6128eaf7bf679
Juan Soriano <juanky@novoda.com>
2022-09-28 09:07:23 -0500

    Revert "Reduce disk space required to save cost"
    This reverts commit 81a08c45d8c739bc83300e6a9e15041782ed2c96.

--- manage-pods.sh
+++ manage-pods.sh
@@ -21,7 +21,7 @@ then
 	curl --request POST \
 	--header 'content-type: application/json' \
 	--url 'https://api.runpod.io/graphql?api_key='$apiKey''  \
-	--data '{"query": "mutation { podFindAndDeployOnDemand( input: { cloudType: ALL, gpuCount: 1,  gpuTypeId: \"NVIDIA RTX A4000\", name: \"RunPod PyTorch\", imageName: \"runpod/pytorch\", dockerArgs: \"bash -c \\\"wget -O - https://raw.githubusercontent.com/novoda/dreams/main/pod-start-command.sh | bash\\\"\", ports: \"8888/http\", volumeMountPath: \"/workspace\", env: [{ key: \"HUGGING_FACE\", value: \"'$huggingFaceToken'\" }] } ) { id imageName env machineId machine { podHostId } } }"}'
+	--data '{"query": "mutation { podFindAndDeployOnDemand( input: { cloudType: ALL, gpuCount: 1, volumeInGb: 5, containerDiskInGb: 5, minVcpuCount: 2, minMemoryInGb: 15, gpuTypeId: \"NVIDIA RTX A4000\", name: \"RunPod PyTorch\", imageName: \"runpod/pytorch\", dockerArgs: \"bash -c \\\"wget -O - https://raw.githubusercontent.com/novoda/dreams/main/pod-start-command.sh | bash\\\"\", ports: \"8888/http\", volumeMountPath: \"/workspace\", env: [{ key: \"HUGGING_FACE\", value: \"'$huggingFaceToken'\" }] } ) { id imageName env machineId machine { podHostId } } }"}'
 fi
 
 
