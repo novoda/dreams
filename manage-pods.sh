24b2646838c4c813dc6e8d78dac8856a6f27938c
Juan Soriano <juanky@novoda.com>
2022-10-04 10:49:45 -0500

    Update scripts for legibility

--- manage-pods.sh
+++ manage-pods.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
-if [ $1 = "--start" ]
-then
-	start=true
-	stop=false
-	apiKey=$2
-	huggingFaceToken=$3
-fi
+operation=$1
+apiKey=$2
+huggingFaceToken=$3
 
-if [ $1 = "--stop" ]
+## START ##
+if [ $operation = "--start" ]
 then
-	start=false
-	stop=true
-	apiKey=$2
-	huggingFaceToken=$3
-fi
-
-if [ $start = true ]
-then
+	## Start a pod with NVIDIA RTX A4000 GPU
 	curl --request POST \
 	--header 'content-type: application/json' \
 	--url 'https://api.runpod.io/graphql?api_key='$apiKey''  \
 	--data '{"query": "mutation { podFindAndDeployOnDemand( input: { cloudType: ALL, gpuCount: 1, volumeInGb: 5, containerDiskInGb: 5, minVcpuCount: 2, minMemoryInGb: 15, gpuTypeId: \"NVIDIA RTX A4000\", name: \"RunPod PyTorch\", imageName: \"runpod/pytorch\", dockerArgs: \"bash -c \\\"wget -O - https://raw.githubusercontent.com/novoda/dreams/main/pod-start-command.sh | bash\\\"\", ports: \"8888/http\", volumeMountPath: \"/workspace\", env: [{ key: \"HUGGING_FACE\", value: \"'$huggingFaceToken'\" }] } ) { id imageName env machineId machine { podHostId } } }"}'
 fi
 
-
-if [ $stop = true ]
+## STOP ##
+if [ $operation = "--stop" ]
 then
+	## Finds running Pod
 	id=$(curl --request POST \
 	--header 'content-type: application/json' \
 	--url 'https://api.runpod.io/graphql?api_key='$apiKey''  \
 	--data '{"query": "query Pods { myself { pods { id } } }"}' | jq -r '.data.myself.pods[0].id')
 
-    curl --request POST \
-    --header 'content-type: application/json' \
-    --url 'https://api.runpod.io/graphql?api_key=ZR0NG1K70SI1TSPMAQ6Y8ZHKBDYH2VMY0WH1LV5T' \
-    --data '{"query": "mutation { podTerminate( input: { podId: \"'$id'\" } ) }"}'
+	## Terminates running Pod
+	curl --request POST \
+	--header 'content-type: application/json' \
+	--url 'https://api.runpod.io/graphql?api_key='$apiKey'' \
+	--data '{"query": "mutation { podTerminate( input: { podId: \"'$id'\" } ) }"}'
 fi
--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 cd "${0%/*}"
+
+## Install NodeJs Package Manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 12
 nvm use 12
+
+## Install local tunnel
 npm install -g localtunnel
+
+## Install Python dependencies (Fast API, PyTorch, ...)
 pip install -r requirements.txt
+
+## Run!!
 python dreams.py & lt --port 8000 --subdomain novoda-dreams 
 
